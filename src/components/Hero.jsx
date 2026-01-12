589338188730d8485a9842d0161f573a62422343
Zino Akpoghelie <zinoakpoghelie17@gmail.com>
2026-01-12 13:49:02 +0100

    change hero image and added overlay

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,26 +2,30 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div>
-      <section
-        className="hero bg-cover bg-center bg-no-repeat h-screen flex items-center justify-center md:justify-start text-white"
-        style={{
-          backgroundImage: "url('/Hero.avif')",
-        }}
-      >
-        <div className="text-center md:text-left md:p-10">
-          <h1 className="text-5xl font-bold mb-4">Welcome to Our Church</h1>
-          <p className="text-xl mb-8 max-w-xl p-5">
-            Join us as we grow in Jesus, serve others, and step boldly into
-            God’s purpose for our lives. Together, we keep moving forward!
-          </p>
-          <div className="flex gap-4 justify-center md:justify-start">
-            <Button text="Visit Us" />
-            <Button text="Learn More" />
-          </div>
+    <section
+      className="relative hero bg-cover bg-center bg-no-repeat h-screen flex items-center justify-center md:justify-start text-white"
+      style={{
+        backgroundImage: "url('/Hero.avif')",
+      }}
+    >
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/40"></div>
+
+      {/* Content */}
+      <div className="relative z-10 text-center md:text-left md:p-10">
+        <h1 className="text-5xl font-bold mb-4">Welcome To Our Church</h1>
+
+        <p className="text-xl mb-8 max-w-xl p-5">
+          Join us as we grow in Jesus, serve others, and step boldly into God’s
+          purpose for our lives. Together, we keep moving forward!
+        </p>
+
+        <div className="flex gap-4 justify-center md:justify-start">
+          <Button text="Visit Us" />
+          <Button text="Learn More" />
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
