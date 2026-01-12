0dbc1da3d22140ad95544197dc3dbf9d69912d59
Zino Akpoghelie <zinoakpoghelie17@gmail.com>
2026-01-12 15:53:20 +0100

    removed bold font from button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <button className="px-4 py-2 flex items-center justify-center rounded-full text-black bg-[#Facf2e] cursor-pointer font-bold">
+    <button className="px-4 py-2 flex items-center justify-center rounded-full text-black bg-[#Facf2e] cursor-pointer">
       {text}
     </button>
   );
